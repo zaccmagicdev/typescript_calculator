03d3a313eac3370ec26a4e762d4e0aa4dbba1966
Zachary Wedel <zwede710@gmail.com>
2024-06-05 20:52:46 -0500

    UPDATE: Making a simple little calculator app to learn TS

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+//variable declarations
+const calcScreen = document.querySelector<HTMLInputElement>('#calculator__screen');
 
-import confetti from 'canvas-confetti';
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+//try making the buttons into objects that can be cloned and return their value to the screen? Could happen with Types in ts
