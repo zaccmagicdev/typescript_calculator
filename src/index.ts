fc51296d2beb59179b131dd94424f98704cf73a4
Zachary Wedel <zwede710@gmail.com>
2024-06-11 17:42:48 -0500

    UPDATE: Code for results history added and works great

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,82 @@
 //variable declarations
 import { evaluate } from 'mathjs';
 
-const calcScreen = document.querySelector<HTMLInputElement>('#calculator__screen');
-const calcButton = document.querySelectorAll<HTMLButtonElement>('.calculator__button');
-const calcResultButton = document.getElementById('calculator__result') as HTMLButtonElement;
-const calcClearButton = document.getElementById('calculator__clear') as HTMLButtonElement;
-const calcHistoryTab = document.getElementById('calculator__history-tab') as HTMLInputElement;
-const calcHistoryList = document.querySelector<HTMLUListElement>('.calculator__history');
+const calcScreen = document.querySelector<HTMLInputElement>(
+  '#calculator__screen',
+);
+const calcButton = document.querySelectorAll<HTMLButtonElement>(
+  '.calculator__button',
+);
+const calcResultButton = document.getElementById(
+  'calculator__result',
+) as HTMLButtonElement;
+const calcClearButton = document.getElementById(
+  'calculator__clear',
+) as HTMLButtonElement;
+const calcHistoryTab = document.getElementById(
+  'calculator__history-tab',
+) as HTMLInputElement;
+const calcHistoryList = document.querySelector<HTMLUListElement>(
+  '.calculator__history',
+);
 
 let calcValue = '';
 calcScreen!.value = '0';
 const exp = /[+-/*.]+/;
 
 type HistoryEntry = {
-    equation: string,
-    answer: string
-}
+  equation: string;
+  answer: string;
+};
 
-calcHistoryTab.checked ? calcHistoryList?.classList.add('calculator__history_shown') : calcHistoryList?.classList.remove('calculator__history_shown');
+calcHistoryTab.addEventListener('change', () => {
+  calcHistoryTab.checked
+    ? calcHistoryList?.classList.add('calculator__history_shown')
+    : calcHistoryList?.classList.remove('calculator__history_shown');
+});
 
 calcButton.forEach((button) => {
-    button.addEventListener('click', (e) => {
-        e.preventDefault();
-        //switch(){}
-        calcValue += button.textContent;
-        calcScreen!.value = calcValue;
-    })
-})
+  button.addEventListener('click', (e) => {
+    e.preventDefault();
+    //switch(){}
+    calcValue += button.textContent;
+    calcScreen!.value = calcValue;
+  });
+});
 
 calcResultButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    calcValue = calcValue.replace(/=/g, '')
-    console.log(calcValue)
-    const currentEquation = calcValue;
-    if(exp.test(calcValue.charAt(calcValue.length - 1))){
-        calcValue = calcValue.substring(0, calcValue.length - 1)
-    } else {
-        const result = evaluate(calcValue);
-        calcScreen!.value = result.toString();
-        //make value global
-        calcValue = result;
-    }
-    const entry = document.createElement("li")
-    const entryContent: HistoryEntry = {
-        equation: currentEquation,
-        answer: calcValue
-    }
-    entry.textContent = `${entryContent.equation} = ${entryContent.answer}`
-    calcHistoryList?.append(entry)
-})
+  e.preventDefault();
+  calcValue = calcValue.replace(/=/g, '');
+  const currentEquation = calcValue;
+  if (exp.test(calcValue.charAt(calcValue.length - 1))) {
+    calcValue = calcValue.substring(0, calcValue.length - 1);
+  } else {
+    const result = evaluate(calcValue);
+    calcScreen!.value = result.toString();
+    //make value global
+    calcValue = result;
+  }
+  const getAnswerButton = document.createElement('button');
+  const entry = document.createElement('li');
+  const entryContent: HistoryEntry = {
+    equation: currentEquation,
+    answer: calcValue,
+  };
+
+  getAnswerButton.addEventListener('click', () => {
+    calcScreen!.value = entryContent.answer;
+    calcValue = entryContent.answer;
+    calcHistoryList?.classList.remove('calculator__history_shown');
+    calcHistoryTab!.checked = false;
+  })
+
+  entry.textContent = `${entryContent.equation} = ${entryContent.answer}`;
+  calcHistoryList?.append(entry);
+  calcHistoryList?.appendChild(getAnswerButton);
+});
 
 calcClearButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    calcValue = '';
-    calcScreen!.value = '0';
-})
+  e.preventDefault();
+  calcValue = '';
+  calcScreen!.value = '0';
+});
